098dd735e800a8a6fd7f73cbeb8939f53328f907
cyrrius <cyrrius@gmail.com>
2021-07-17 12:53:03 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOdd(10, 5));
+    }
+
+    public static boolean isOdd(int number){
+        if(number <= 0) {
+            return false;
+        }
+
+        if(number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static int sumOdd(int start, int end) {
+
+        if((end < start) || (end < 0) || (start < 0)) {
+            return -1;
+        }
+
+        int sum = 0;
+        for(int i = start; i <= end; i++) {
+            if(isOdd(i)) {
+                sum += i;
+
+            }
+        }
+        return sum;
+
+    }
+}
